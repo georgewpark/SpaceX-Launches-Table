48538c8c358274cf616d1d866b3f9ce601f43693
George Park <georgewpark@outlook.com>
2022-10-06 12:19:33 +0100

    move components to components folder

--- int-src/js/app.js
+++ int-src/js/app.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import Table from './Table'
+import Table from './components/Table'
 
 const container = document.querySelector('#launches-table')
 const root = container && createRoot(container)
